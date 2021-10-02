80f8edc14f3382fdc94807ec6e95bfd3cc984401
Harpreet Kaur <59807874+HarpreetKaurNait@users.noreply.github.com>
2021-10-02 10:40:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_found
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 3, 4, 5 };
+            Console.Write("enter:");
+            int.TryParse(Console.ReadLine(), out int  search);
+            int found = Array.BinarySearch(array, search);
+            Console.WriteLine($"found={found}");
+            Console.ReadKey();
+        }
+    }
+}
